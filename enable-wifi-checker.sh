2d8057c330d744692919e23ab361c9c41e2ac16f
mateusz <mateusz.jaje@grandparade.co.uk>
2016-03-17 16:04:45 +0100

    initial, wifi checker

---
+++ enable-wifi-checker.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+nohup ~/bin/ensure-wifi.py >> ~/logs/ensure-wifi.log &
+
---
+++ ensure-wifi.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+from subprocess import run, PIPE
+from time import sleep
+
+CONNECT_TIMEOUT = '1'
+CHECK_TIMEOUT = 5
+WAIT_FOR_TURNING_OFF = 2
+WAIT_FOR_TURNING_ON = 15
+
+code = 0
+debug = False
+
+
+def execute_req():
+    cmd = ['curl', '-X', 'HEAD', '--connect-timeout', CONNECT_TIMEOUT, 'www.wp.pl']
+    if debug:
+        print(' '.join(cmd))
+    return run(cmd, stdout=PIPE, stderr=PIPE)
+
+
+while True:
+    while code == 0:
+        result = execute_req()
+        code = result.returncode
+        if debug:
+            print('Checking internet connection end with code %d and stdout %s and stderr %s' % (
+            code, repr(result.stdout), repr(result.stderr)))
+        sleep(CHECK_TIMEOUT)
+
+    result = run(['networksetup', '-setairportpower', 'en0', 'off'], stdout=PIPE, stderr=PIPE)
+    print('Turning off Wi-Fi end with code %d\n\tstdout: %s\n\tstderr: %s' % (
+        result.returncode, repr(result.stdout), repr(result.stderr)))
+    sleep(WAIT_FOR_TURNING_OFF)
+    result = run(['networksetup', '-setairportpower', 'en0', 'on'], stdout=PIPE, stderr=PIPE)
+    print('Turning on Wi-Fi end with code %d\n\tstdout: %s\n\tstderr: %s' % (
+        result.returncode, repr(result.stdout), repr(result.stderr)))
+    sleep(WAIT_FOR_TURNING_ON)
+    code = execute_req().returncode

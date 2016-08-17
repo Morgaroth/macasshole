4f05e8c4421ce28ad9f5f3939add47ad4d8e4076
Mateusz Jaje <Morgaroth@users.noreply.github.com>
2016-08-17 10:04:50 +0200

    fixed

--- enable-wifi-disabler.sh
+++ enable-wifi-disabler.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-nohup ~/prv-projects/macasshole/disable_wifi_when_thunderbolt.py >> ~/logs/disable-wifi.log &
+nohup /Users/mateusz/prv-projects/macasshole/disable_wifi_when_thunderbolt.py &> /Users/mateusz/logs/disable-wifi.log 2>&1&
 
